fe93b9b265c44bb42cacf562f4bcc26c7f1b2374
legbeard <au484146@uni.au.dk>
2019-09-12 14:04:33 +0100

    Fixed the test that Martin broke

--- Test.ECS.Legacy/TestLegacy.cs
+++ Test.ECS.Legacy/TestLegacy.cs
@@ -11,7 +11,20 @@ namespace Test.ECS.Legacy
     [TestFixture]
     class TestLegacy
     {
-        
+        private global::ECS.Legacy.ECS uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new global::ECS.Legacy.ECS(10);
+        }
+
+        [Test]
+        public void RunSelfTest_CallRunSelfTest_True()
+        {
+            Assert.That(uut.RunSelfTest,Is.True);
+        }
+
         static void Main(string[] args)
         {
 
