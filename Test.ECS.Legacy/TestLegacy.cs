40c6ebca7c749ec5ded5edd1b00cec94446cd2c9
martinsekd <201710685@post.au.dk>
2019-09-12 14:00:43 +0200

    Something something test

--- Test.ECS.Legacy/TestLegacy.cs
+++ Test.ECS.Legacy/TestLegacy.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ECS.Legacy;
+using NUnit.Framework;
 
 namespace Test.ECS.Legacy
 {
+    [TestFixture]
     class TestLegacy
     {
-        
+        private global::ECS.Legacy.ECS uut;
+        [SetUp]
+        public void Setup()
+        {
+            uut = new global::ECS.Legacy.ECS(10);
+        }
+
+        [Test]
+        public void TEST_TEST_TEST()
+        {
+            Assert.That(0,Is.EqualTo(0));
+        }
         static void Main(string[] args)
         {
 
