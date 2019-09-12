a3bda39a669b895ebcf159af20c32083d007d999
legbeard <au484146@uni.au.dk>
2019-09-12 13:34:41 +0100

    Added a test tester to make jenkins work

--- Test.ECS.Legacy/TestLegacy.cs
+++ Test.ECS.Legacy/TestLegacy.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ECS.Legacy;
+using NUnit.Framework;
 
 namespace Test.ECS.Legacy
 {
     class TestLegacy
     {
-        
+        [Test]
+        public void Test_Test()
+        {
+            Assert.That(0, Is.Zero);
+        }
         static void Main(string[] args)
         {
 
