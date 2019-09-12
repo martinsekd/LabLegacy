47eb0a779b80acc5853e9919a403bf39e1cd2718
martinsekd <201710685@post.au.dk>
2019-09-12 13:39:45 +0200

    Fake implementations

---
+++ ECS.Legacy/FakeHeater.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Legacy
+{
+    class FakeHeater : IHeater
+    {
+        private bool turnOn_;
+
+        public void TurnOn()
+        {
+            turnOn_ = true;
+        }
+        
+
+        public void TurnOff()
+        {
+            turnOn_ = false;
+        }
+
+        public bool RunSelfTest()
+        {
+            return turnOn_;
+        }
+    }
+}
